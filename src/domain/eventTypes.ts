bf127acbfe41fba908ee5e626b4d7b0c00e00b6d
snoader <pmdanielschneider@gmail.com>
2026-02-27 18:16:57 +0100

    refactor(domain): centralize event type definitions in events module

--- src/domain/eventTypes.ts
+++ src/domain/eventTypes.ts
@@ -1,23 +1,5 @@
-export const MATCH_EVENT_TYPES = [
-  "match_start",
-  "next_turn",
-  "turn_set",
-  "half_changed",
-  "touchdown",
-  "completion",
-  "interception",
-  "injury",
-  "casualty",
-  "ko",
-  "foul",
-  "turnover",
-  "kickoff",
-  "kickoff_event",
-  "weather_set",
-  "reroll_used",
-  "apothecary_used",
-  "prayer_result",
-  "note",
-] as const;
-
-export type MatchEventType = (typeof MATCH_EVENT_TYPES)[number];
+/**
+ * @deprecated Import event typing from "./events" instead.
+ */
+export { MATCH_EVENT_TYPES } from "./events";
+export type { EventType as MatchEventType } from "./events";
