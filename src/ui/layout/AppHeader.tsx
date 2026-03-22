d2b8afea8067c2dba3cd391bf18f2d897b3f08cf
snoader <pmdanielschneider@gmail.com>
2026-03-22 20:45:37 +0100

    Patch visible app name and bump version

--- src/ui/layout/AppHeader.tsx
+++ src/ui/layout/AppHeader.tsx
@@ -3,7 +3,7 @@ import { HamburgerMenu } from "./HamburgerMenu";
 export function AppHeader() {
   return (
     <header className="app-header">
-      <div className="app-header-title">BB Match Notes</div>
+      <div className="app-header-title">Blood Bowl Note Taker</div>
       <HamburgerMenu />
     </header>
   );
